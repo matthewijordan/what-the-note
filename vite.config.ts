97bec32a2126bb3ee14f8ae6625b2ffb3c3c07c2
Matt🤠 <matt@enhancelabs.ai>
2025-10-01 15:44:08 -0700

    Release v0.1.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -25,6 +26,15 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+  // Include both HTML files in the build
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        preferences: resolve(__dirname, "preferences.html"),
+      },
     },
   },
 }));
